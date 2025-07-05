6a472950dd46b76f43f46dae3d2310bf03fc8c1c
truongkien03 <hello2k3.2k3@gmail.com>
2025-07-05 20:03:49 +0700

    firebase

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,6 @@
 class AppConfig {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl =
+      'https://caring-talented-slug.ngrok-free.app/api';
 
   // Driver API endpoints
   static const String driverRegisterOtp = '/driver/register/otp';
